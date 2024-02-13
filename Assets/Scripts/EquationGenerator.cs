29434bca4f8225337fa6d95cf319ceb9f9e6bf02
Regames99 <kengonosyoutai@gmail.com>
2024-02-14 00:28:57 +0900

    no message

---
+++ Assets/Scripts/EquationGenerator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EquationGenerator : MonoBehaviour
+{
+
+    [SerializeField] TMP_InputField equationInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
